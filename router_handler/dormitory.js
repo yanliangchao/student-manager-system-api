633806ed28947779a8f9a77c4fe88e9c17fd5368
yanliangchao <liangchao.yan@outlook.com>
2024-06-05 21:57:27 +0800

    实现打印功能

--- router_handler/dormitory.js
+++ router_handler/dormitory.js
@@ -96,7 +96,7 @@ exports.getStuByid = async (req, res) => {
     try {
         const id = req.params.id
         let result;
-        const sql2 = `select stu.id, stu.name, stu.address, stu.iphone, stu.father, stu.father_iphone, stu.mother, stu.mother_iphone, 
+        const sql2 = `select stu.id, stu.name, stu.gender, stu.address, stu.iphone, stu.father, stu.father_iphone, stu.mother, stu.mother_iphone, 
                         tsd.number, tsd.times, tsd.owner, tcs.id cid, tcs.class_id, tcs.class_name from t_student_dormitory tsd 
                         left join t_student stu on tsd.sid = stu.id
                         left join t_class tcs on stu.cid = tcs.id
@@ -142,6 +142,156 @@ exports.pingfen = async (req, res) => {
         // 循环学生，将公共评分加入学生详情，
         for(const public of pingfen.publics) {
             for (const student of students) {
+                const sql2 = `insert into t_student_details (sid, number, describes, type, times, uid) values ($1, $2, $3, $4, $5, $6)`
+                await db.query(sql2, [student.sid, public.number, public.reason, 0, new Date(), user.id]);
+            }
+        }
+        // 有个人部分也加个人部分
+        for(const personal of pingfen.personals) {
+            for(const public of personal.publics) {
+                const sql3 = `insert into t_student_details (sid, number, describes, type, times, uid) values ($1, $2, $3, $4, $5, $6)`
+                await db.query(sql3, [personal.sid, public.number, public.reason, 1, new Date(), user.id]);
+            } 
+        }
+        res.json({
+            status: 200,
+            message: "新增成功",
+        });
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+exports.dianming = async (req, res) => {
+    try {
+        const user = await jwt.decode(req)
+        const dianming = req.body
+        // 请假
+        for(const leave of dianming.leaves) {
+            const sql1 = `insert into t_student_details (sid, number, describes, type, times, uid) values ($1, $2, $3, $4, $5, $6)`
+            await db.query(sql1, [leave, 0, "请假", 1, new Date(), user.id]);
+        }
+        // 缺寝
+        for(const absenc of dianming.absence) {
+            const sql2 = `insert into t_student_details (sid, number, describes, type, times, uid) values ($1, $2, $3, $4, $5, $6)`
+            await db.query(sql2, [absenc, 2, "缺寝", 1, new Date(), user.id]);
+        }
+        res.json({
+            status: 200,
+            message: "新增成功",
+        });
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+exports.getStorey = async (req, res) => {
+    try {
+        const requestParams = req.query;
+        let pageIndex = (requestParams.pageNum - 1) * requestParams.pageSize
+        let pageCount = requestParams.pageSize
+        // 查询楼层信息
+        const sql1 = `SELECT tdm.building, tdm.storey, COUNT(*) count FROM t_dormitory tdm GROUP BY tdm.building, tdm.storey limit $1 offset $2`;
+        const response = await db.query(sql1, [pageCount, pageIndex]);
+        const storeys = response.rows;
+        // 查询楼层数量
+        const sql2 = `select count(*) FROM (SELECT tdm.building, tdm.storey, COUNT(*) count FROM t_dormitory tdm GROUP BY tdm.building, tdm.storey)`
+        const countResponse = await db.query(sql2);
+        const count = countResponse.rows[0].count
+       
+        res.json({
+            status: 200,
+            message: "查询成功",
+            count: count,
+            data: storeys,
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+exports.pingfenPrint = async (req, res) => {
+    try {
+        const user = await jwt.decode(req)
+        // 查询学校
+        const sql = `select * from t_school where id= $1`;
+        const schoolResponse = await db.query(sql, [user.sid]);
+        const school = schoolResponse.rows[0];
+
+        const storey = req.query
+        // 查询楼层全部宿舍
+        const sql1 = `select tdm.* from t_dormitory tdm where tdm.building = $1 and tdm.storey = $2 order by tdm.name asc`;
+        const response = await db.query(sql1, [storey.building, storey.storey]);
+        const tdms = response.rows;
+        const result = []
+        for(const tdm of tdms) {
+            // 生成数据
+            const sql1 = `select tsu.name, tsd.describes, tsd.type, tsd.times, tsd.number from t_student_details tsd 
+                    left join t_student tsu on tsd.sid = tsu.id 
+                    left join t_student_dormitory tsdm on tsd.sid = tsdm.sid
+                    where tsdm.did = $1 and tsd.number > 0 and date_trunc('day', tsd.times) = $2`;
+            const detailResponse = await db.query(sql1, [tdm.id, storey.times]);
+            const details = detailResponse.rows;
+            let data = {
+                name: tdm.name,
+                morning: {
+                    public: [],
+                    personal: []
+                },
+                afternoon: {
+                    public: [],
+                    personal: []
+                }
+            }
+            for(const detail of details) {
+                const date = new Date(detail.times);
+                if(date.getUTCHours() > 0 && date.getUTCHours() < 11) {
+                    if(detail.type == 0) {
+                        if(!data.morning.public.includes(detail.describes + " -" + detail.number)) {
+                            data.morning.public.push(detail.describes + " -" + detail.number)
+                        }
+                    } else {
+                        data.morning.personal.push(detail.name + ": " + detail.describes + " -" + detail.number)
+                    }
+                } else {
+                    if(detail.type == 0) {
+                        if(!data.afternoon.public.includes(detail.describes + " -" + detail.number)) {
+                            data.afternoon.public.push(detail.describes + " -" + detail.number)
+                        }
+                    } else {
+                        data.afternoon.personal.push(detail.name + ": " + detail.describes + " -" + detail.number)
+                    }
+                }
+            }
+            result.push(data)
+        }
+
+        res.json({
+            status: 200,
+            message: "查询成功",
+            data: result,
+            school: school.school_name,
+        });
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+
+exports.dianmingPrint = async (req, res) => {
+    try {
+        const user = await jwt.decode(req)
+        const pingfen = req.body
+        // 查询宿舍的全部学生
+        const sql1 = `select tsd.sid from t_student_dormitory tsd where tsd.did = $1`;
+        const response = await db.query(sql1, [pingfen.did]);
+        const students = response.rows;
+        // 循环学生，将公共评分加入学生详情，
+        for(const public of pingfen.publics) {
+            for (const student of students) {
                 const sql2 = `insert into t_student_details (sid, number, describes, times, uid) values ($1, $2, $3, $4, $5)`
                 await db.query(sql2, [student.sid, public.number, public.reason + "卫生扣分", new Date(), user.id]);
             }
@@ -162,30 +312,6 @@ exports.pingfen = async (req, res) => {
         res.status(400).json(err);
     }
 }
-
-exports.dianming = async (req, res) => {
-    try {
-        const user = await jwt.decode(req)
-        const dianming = req.body
-        // 请假
-        for(const leave of dianming.leaves) {
-            const sql1 = `insert into t_student_details (sid, number, describes, times, uid) values ($1, $2, $3, $4, $5)`
-            await db.query(sql1, [leave, 0, "请假", new Date(), user.id]);
-        }
-        // 缺寝
-        for(const absenc of dianming.absence) {
-            const sql2 = `insert into t_student_details (sid, number, describes, times, uid) values ($1, $2, $3, $4, $5)`
-            await db.query(sql2, [absenc, 2, "缺寝", new Date(), user.id]);
-        }
-        res.json({
-            status: 200,
-            message: "新增成功",
-        });
-        
-    } catch (err) {
-        res.status(400).json(err);
-    }
-};
 
 exports.addStu = async (req, res) => {
     try {
